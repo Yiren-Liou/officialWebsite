0048b380d154d71a6bc417257e5a4fb6c0f88370
Yiren <yirenliou@lafresh.com.tw>
2023-11-18 22:51:45 +0800

    [新增] install gh-page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/officialWebsite/',
+  base:process.env.NODE_ENV === 'production'? '/officialWebsite/' : '/',
   plugins: [vue()],
 })
